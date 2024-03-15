eb01a963ce5a7a37908f5f89da93354c3610d08b
Lewis Barnett <lewis.barnett@noaa.gov>
2024-03-15 12:15:49 -0700

    split into script and functions

---
+++ Code/functions.R
@@ -0,0 +1,127 @@
+# simulate observations of sea ice and cold pool
+simulateX <- function(object, nsim = 1, seed = NULL, X, ...) {
+  object$fitted.values <- predict(object, X)
+  simulate(object = object, nsim = nsim, seed = seed, ...)
+}
+
+# Function to determine the operating model based on sea ice value
+get_operating_model <- function(cold_pool_value, params) {
+  if (cold_pool_value >= high_cp[1] && cold_pool_value <= high_cp[2]) {
+    sim_dat <- sdmTMB_simulate(
+      formula = ~ 1 + depth,
+      data = predictor_dat,
+      mesh = mesh,
+      family = gaussian(link = "identity"),
+      B = c(0.1, params$B1_high), # B0 = intercept, B1 = depth coefficient slope
+      range = params$range,
+      sigma_O = 0.2,
+      phi = params$phi # SD of observation error in this (Gaussian) case
+    )
+    
+    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
+    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
+    
+    return(sim_dat)
+    
+  } else if (cold_pool_value >= mid_cp[1] && cold_pool_value <= mid_cp[2]) {
+    sim_dat <- sdmTMB_simulate(
+      formula = ~ 1 + depth,
+      data = predictor_dat,
+      mesh = mesh,
+      family = gaussian(link = "identity"),
+      B = c(0.1, params$B1_mid), # B0 = intercept, B1 = depth coefficient slope
+      range = params$range,
+      sigma_O = 0.2,
+      phi = params$phi # SD of observation error in this (Gaussian) case
+    )
+    
+    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
+    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
+    
+    return(sim_dat)
+    
+  } else if (cold_pool_value >= 0 && cold_pool_value <= low_cp[2]) {
+    sim_dat <- sdmTMB_simulate(
+      formula = ~ 1 + depth,
+      data = predictor_dat,
+      mesh = mesh,
+      family = gaussian(link = "identity"),
+      B = c(0.1, params$B1_low), # B0 = intercept, B1 = depth coefficient slope
+      range = params$range,
+      sigma_O = 0.2,
+      phi = params$phi # SD of observation error in this (Gaussian) case
+    )
+    
+    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
+    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
+    
+    return(sim_dat)
+    
+  } else {
+    return("No Matching Operating Model")
+  }
+}
+
+# Simulate sampling
+abundance <- function(d, ice_value, n) {
+  
+  d$strata <- ifelse(d$Y >= 50, 1, 2) # TODO: redefine strata (not evenly split somehow?)
+  
+  if (ice_value >= high_ice[1] && ice_value <= high_ice[2]) {
+    samples_north <- sample(d[d$strata == 1, "observed"], n*0.1)
+    samples_south <- sample(d[d$strata == 2, "observed"], n*0.9)
+    
+  } else if (ice_value >= mid_ice[1] && ice_value <= mid_ice[2]) {
+    samples_north <- sample(d[d$strata == 1, "observed"], n*0.25)
+    samples_south <- sample(d[d$strata == 2, "observed"], n*0.75)
+    
+  } else if (ice_value >= low_ice[1] && ice_value <= low_ice[2]) {
+    samples_north <- sample(d[d$strata == 1, "observed"], n*0.5)
+    samples_south <- sample(d[d$strata == 2, "observed"], n*0.5)
+  }
+  
+  # estimate abundance
+  est <- sum(mean(samples_north) * nrow(d[d$strata == 1,]), 
+             mean(samples_south) * nrow(d[d$strata == 2,])
+  )
+  
+  # true abundance
+  truth <- sum(d$eta)
+  
+  return(as.data.frame(cbind(n, ice_value, est, truth)))
+}
+
+# simulate observations for plotting
+sampling <- function(d, ice_value, n) {
+  # Define strata based on Y values
+  d$strata <- ifelse(d$Y >= 50, 1, 2)
+  
+  # Define sampling proportions based on ice_value
+  if (ice_value >= high_ice[1] && ice_value <= high_ice[2]) {
+    prop_north <- 0.1
+    prop_south <- 0.9
+  } else if (ice_value >= mid_ice[1] && ice_value <= mid_ice[2]) {
+    prop_north <- 0.25
+    prop_south <- 0.75
+  } else if (ice_value >= low_ice[1] && ice_value <= low_ice[2]) {
+    prop_north <- 0.5
+    prop_south <- 0.5
+  }
+  
+  # Sample row indices for north and south strata
+  indices_north <- sample(which(d$strata == 1), floor(n * prop_north))
+  indices_south <- sample(which(d$strata == 2), floor(n * prop_south))
+  
+  # Extract sampled rows from the data frame
+  samples_north <- d[indices_north, ]
+  samples_south <- d[indices_south, ]
+  
+  # Add a region column to each data frame
+  samples_north$region <- "north"
+  samples_south$region <- "south"
+  
+  # Combine the data frames into a single data frame
+  combined_samples <- dplyr::bind_rows(samples_north, samples_south)
+  
+  return(combined_samples)
+}
--- Code/sim.R
+++ Code/sim.R
@@ -1,103 +1,10 @@
+source("Code/functions.R")
 library(sdmTMB)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(reshape2)
 
-# COLD POOL SIM FUNCTIONS ----
-simulateX <- function(object, nsim = 1, seed = NULL, X, ...) {
-  object$fitted.values <- predict(object, X)
-  simulate(object = object, nsim = nsim, seed = seed, ...)
-}
-
-# Function to determine the operating model based on sea ice value
-get_operating_model <- function(cold_pool_value, params) {
-  if (cold_pool_value >= high_cp[1] && cold_pool_value <= high_cp[2]) {
-    sim_dat <- sdmTMB_simulate(
-      formula = ~ 1 + depth,
-      data = predictor_dat,
-      mesh = mesh,
-      family = gaussian(link = "identity"),
-      B = c(0.1, params$B1_high), # B0 = intercept, B1 = depth coefficient slope
-      range = params$range,
-      sigma_O = 0.2,
-      phi = params$phi # SD of observation error in this (Gaussian) case
-    )
-    
-    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
-    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
-    
-    return(sim_dat)
-    
-  } else if (cold_pool_value >= mid_cp[1] && cold_pool_value <= mid_cp[2]) {
-    sim_dat <- sdmTMB_simulate(
-      formula = ~ 1 + depth,
-      data = predictor_dat,
-      mesh = mesh,
-      family = gaussian(link = "identity"),
-      B = c(0.1, params$B1_mid), # B0 = intercept, B1 = depth coefficient slope
-      range = params$range,
-      sigma_O = 0.2,
-      phi = params$phi # SD of observation error in this (Gaussian) case
-    )
-    
-    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
-    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
-    
-    return(sim_dat)
-    
-  } else if (cold_pool_value >= 0 && cold_pool_value <= low_cp[2]) {
-    sim_dat <- sdmTMB_simulate(
-      formula = ~ 1 + depth,
-      data = predictor_dat,
-      mesh = mesh,
-      family = gaussian(link = "identity"),
-      B = c(0.1, params$B1_low), # B0 = intercept, B1 = depth coefficient slope
-      range = params$range,
-      sigma_O = 0.2,
-      phi = params$phi # SD of observation error in this (Gaussian) case
-    )
-    
-    sim_dat$eta <- (sim_dat$eta - min(sim_dat$eta))
-    sim_dat$observed <- (sim_dat$observed - min(sim_dat$observed))
-    
-    return(sim_dat)
-    
-  } else {
-    return("No Matching Operating Model")
-  }
-}
-
-# Simulate sampling
-abundance <- function(d, ice_value, n) {
-  
-  d$strata <- ifelse(d$Y >= 50, 1, 2) # TODO: redefine strata (not evenly split somehow?)
-  
-  if (ice_value >= high_ice[1] && ice_value <= high_ice[2]) {
-    samples_north <- sample(d[d$strata == 1, "observed"], n*0.1)
-    samples_south <- sample(d[d$strata == 2, "observed"], n*0.9)
-    
-  } else if (ice_value >= mid_ice[1] && ice_value <= mid_ice[2]) {
-    samples_north <- sample(d[d$strata == 1, "observed"], n*0.25)
-    samples_south <- sample(d[d$strata == 2, "observed"], n*0.75)
-    
-  } else if (ice_value >= low_ice[1] && ice_value <= low_ice[2]) {
-    samples_north <- sample(d[d$strata == 1, "observed"], n*0.5)
-    samples_south <- sample(d[d$strata == 2, "observed"], n*0.5)
-  }
-  
-  # estimate abundance
-  est <- sum(mean(samples_north) * nrow(d[d$strata == 1,]), 
-             mean(samples_south) * nrow(d[d$strata == 2,])
-  )
-  
-  # true abundance
-  truth <- sum(d$eta)
-  
-  return(as.data.frame(cbind(n, ice_value, est, truth)))
-}
-
-
-# Sea Ice - Cold pool simulations ----
 set.seed(123)
 
 # Sample size
@@ -195,3 +102,184 @@ for(i in 1:nrow(params)){
 
 results
 #saveRDS(results, "results.RDS")
+
+
+# Plots for cold pool sea ice simulations -----
+#results <- readRDS("results.RDS") #load results
+
+# plot bias and RRMSE of abundance estimates, grouped by scenario ----
+p_range_bias <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  ggplot(aes(as.factor(range), bias)) + 
+  geom_boxplot() +
+  labs(x = "Spatial Range", y = "Bias") +
+  theme_bw()
+p_range_bias
+ggsave("Figures/SimFigs/range_bias.pdf")
+
+p_range_rrmse <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  group_by(range) %>%
+  summarise(rrmse = (sqrt(mean(bias ^ 2)) / mean(est)) * 100) %>%
+  ungroup() %>%
+  ggplot(aes(range, rrmse)) + 
+  geom_point() +
+  geom_line() +
+  labs(x = "Spatial Range", y = "RRMSE %") +
+  theme_bw()
+p_range_rrmse
+ggsave("Figures/SimFigs/range_rrmse.pdf")
+
+p_obserr_bias <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  ggplot(aes(as.factor(phi), bias)) + 
+  geom_boxplot() +
+  labs(x = "Observation Error SD", y = "Bias") +
+  theme_bw()
+p_obserr_bias
+ggsave("Figures/SimFigs/obserr_bias.pdf")
+
+p_obserr_rrmse <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  group_by(phi) %>%
+  summarise(rrmse = (sqrt(mean(bias ^ 2)) / mean(est)) * 100) %>%
+  ungroup() %>%
+  ggplot(aes(phi, rrmse)) + 
+  geom_point() +
+  geom_line() +
+  labs(x = "Observation Error SD", y = "RRMSE %") +
+  theme_bw()
+p_obserr_rrmse
+ggsave("Figures/SimFigs/obserr_rrmse.pdf")           
+
+p_gradient_bias <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  ggplot(aes(as.factor(B1_low), bias)) + 
+  geom_boxplot() +
+  labs(x = "True Population Density Gradient", y = "Bias") +
+  theme_bw()
+p_gradient_bias
+ggsave("Figures/SimFigs/gradient_bias.pdf")
+
+p_gradient_rrmse <- drop_na(results) %>% 
+  mutate(bias = est - truth) %>%
+  group_by(B1_low) %>%
+  summarise(rrmse = (sqrt(mean(bias ^ 2)) / mean(est)) * 100) %>%
+  ungroup() %>%
+  ggplot(aes(B1_low, rrmse)) + 
+  geom_point() +
+  geom_line() +
+  labs(x = "True Population Density Gradient", y = "RRMSE %") +
+  theme_bw()
+p_gradient_rrmse
+ggsave("Figures/SimFigs/gradient_rrmse.pdf")
+
+
+# plot estimate and truth across parameters ----
+
+# # exploratory
+# #range and estimate
+# ggplot(data = results, aes(x = range, y = est, group = range)) +
+#   geom_boxplot()
+# #range and truth
+# ggplot(data = results, aes(x = range, y = truth, group = range)) +
+#   geom_boxplot()
+
+#range
+results_range <- select(results, range, est, truth)
+long_results_r <- melt(results_range, id.vars = "range")
+
+ggplot(long_results_r,aes(range,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+#ggsave("Figures/SimFigs/rangecomp.pdf")
+
+#phi
+results_phi <- select(results, phi, est, truth)
+long_results_p <- melt(results_phi, id.vars = "phi")
+
+ggplot(long_results_p,aes(phi,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+
+#b1_low
+results_b1 <- select(results, B1_low, est, truth)
+long_results_b1 <- melt(results_b1, id.vars = "B1_low")
+
+ggplot(long_results_b1,aes(B1_low,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+
+#b1_mid
+results_bm <- select(results, B1_mid, est, truth)
+long_results_bm <- melt(results_bm, id.vars = "B1_mid")
+
+ggplot(long_results_bm,aes(B1_mid,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+
+#b1_high
+results_bh <- select(results, B1_high, est, truth)
+long_results_bh <- melt(results_bh, id.vars = "B1_high")
+
+ggplot(long_results_bh,aes(B1_high,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+
+#ice_value
+results_ice <- select(results, ice_value, est, truth)
+long_results_ice <- melt(results_ice, id.vars = "ice_value")
+
+ggplot(long_results_ice,aes(ice_value,value,fill=variable))+
+  geom_bar(stat="identity",position="dodge")
+
+
+# plot operating model map with observations ----
+
+#representative range values
+op_low <- get_operating_model(100000, params[16,])
+sim_dat <- sampling(op_low,0.5,100)
+
+range20 <- ggplot(op_low, aes(X, Y)) +
+  geom_raster(aes(fill = eta)) +
+  geom_point(aes(size = observed), data = sim_dat, pch = 21) +
+  geom_hline(aes(yintercept = 50)) +
+  scale_fill_viridis_c() +
+  scale_size_area() +
+  coord_cartesian(expand = FALSE)
+ggsave("Figures/SimFigs/range20_plot.png", range20)
+
+params[20,]
+op_low <- get_operating_model(100000, params[20,])
+sim_dat <- sampling(op_low,0.5,100)
+
+range100 <- ggplot(op_low, aes(X, Y)) +
+  geom_raster(aes(fill = eta)) +
+  geom_point(aes(size = observed), data = sim_dat, pch = 21) +
+  geom_hline(aes(yintercept = 50)) +
+  scale_fill_viridis_c() +
+  scale_size_area() +
+  coord_cartesian(expand = FALSE)
+ggsave("Figures/SimFigs/range100_plot.png", range100)
+
+#representative density values
+params[13,]
+op_low <- get_operating_model(100000, params[13,])
+sim_dat <- sampling(op_low,0.5,100)
+
+wk_grad <- ggplot(op_low, aes(X, Y)) +
+  geom_raster(aes(fill = eta)) +
+  geom_point(aes(size = observed), data = sim_dat, pch = 21) +
+  geom_hline(aes(yintercept = 50)) +
+  scale_fill_viridis_c() +
+  scale_size_area() +
+  coord_cartesian(expand = FALSE)
+ggsave("Figures/SimFigs/wk_grad_plot.png", wk_grad)
+
+params[88,]
+op_low <- get_operating_model(100000, params[88,])
+sim_dat <- sampling(op_low,0.5,100)
+
+strng_grad <- ggplot(op_low, aes(X, Y)) +
+  geom_raster(aes(fill = eta)) +
+  geom_point(aes(size = observed), data = sim_dat, pch = 21) +
+  geom_hline(aes(yintercept = 50)) +
+  scale_fill_viridis_c() +
+  scale_size_area() +
+  coord_cartesian(expand = FALSE)
+ggsave("Figures/SimFigs/strng_grad_plot.png", strng_grad)
